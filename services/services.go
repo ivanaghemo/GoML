e8e159c74d634e2a8a099aee5e787630c09874d8
ivanaghemo <70296367+ivanaghemo@users.noreply.github.com>
2021-04-25 18:26:14 -0300

    Servicios

--- services/services.go
+++ services/services.go
@@ -3,9 +3,16 @@ package services
 import (
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/patrickmn/go-cache"
 	operations "github.com/ivanaghemo/GoML/operations"
 	structs "github.com/ivanaghemo/GoML/structs"
+)
+
+var (
+	c        = cache.New(5*time.Minute, 10*time.Minute)
+	cachekey = "cache/topsecret_split"
 )
 
 func TopSecretService(req structs.TopSecretRequest) (*structs.TopSecretResponse, error) {
@@ -48,5 +55,67 @@ func TopSecretService(req structs.TopSecretRequest) (*structs.TopSecretResponse,
 	}
 
 	log.Println("[FINAL] topSecretService")
+	return &res, nil
+}
+
+func TopSecretSplitPostService(nombreSatelite string, req structs.TopSecretRequestSplit) (*structs.TopSecretSplitResponse, error) {
+
+	satelite, err := operations.ObtenerUnSatelite(nombreSatelite)
+
+	if err != nil {
+		msg := fmt.Errorf("No se puede ejecutar la misión %s", err.Error())
+		return nil, msg
+	}
+
+	satelite.Distance = req.Distance
+	satelite.Message = req.Message
+
+	operations.MofificarSatelite(satelite)
+
+	res := structs.TopSecretSplitResponse{
+		Message: fmt.Sprintf("Ok"),
+	}
+
+	return &res, nil
+}
+
+func TopSecretSplitGetService() (*structs.TopSecretResponse, error) {
+
+	//cache
+	if x, found := c.Get(cachekey); found {
+		ress := x.(*structs.TopSecretResponse)
+		log.Println("Cache...")
+		return ress, nil
+	}
+
+	satelites := operations.GetSatelitesOnlineSplit()
+
+	distances, msgs := operations.ObtenerMensajeYDistancia(satelites)
+
+	x, y := operations.GetLocationSplit(distances...)
+
+	if x == 0 && y == 0 {
+		msg := fmt.Errorf("No se puede ubicar la nave o los satétiles no están en línea.")
+		return nil, msg
+	}
+
+	message := operations.GetMessage(msgs...)
+
+	if message == "" {
+		msg := fmt.Errorf("Mensaje incompleto")
+
+		return nil, msg
+	}
+
+	res := structs.TopSecretResponse{
+		Position: structs.Point{
+			X: x,
+			Y: y,
+		},
+		Message: message,
+	}
+
+	c.Set(cachekey, &res, cache.DefaultExpiration)
+
 	return &res, nil
 }
